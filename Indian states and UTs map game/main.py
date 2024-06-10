04e5c394e580aff28ec4fc8eb6b7f9e390e9fbf0
Abinashbora1 <abinashbora01@gmail.com>
2024-06-11 04:33:31 +0530

    Dashboard

---
+++ Indian states and UTs map game/main.py
@@ -0,0 +1,33 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("India States & UTs Game")
+image = "india_map.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("States and Union Territories of India.csv")
+
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/36 States & UTs Correct",
+                                    prompt="What's another state's or UTs name?").title()
+    if answer_state == "Exit":
+        missing_states = [state for state in all_states if state not in guessed_states]
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x.iloc[0]), int(state_data.y.iloc[0]))
+        t.write(answer_state)
